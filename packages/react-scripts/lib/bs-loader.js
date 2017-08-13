91c92b2d4d7b33e2bedbb6645093360b02d5988c
Nick Zalutskiy <nick@zinzout.com>
2017-08-12 23:33:39 -0400

    Fix bs loader crash on missing sources.

--- packages/react-scripts/lib/bs-loader.js
+++ packages/react-scripts/lib/bs-loader.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const fs = require('fs-extra');
 const child_process = require('child-process-promise');
+const chalk = require('chalk');
 const ChildProcessError = require('child-process-promise/lib/ChildProcessError');
 const { getOptions } = require('loader-utils');
 
@@ -146,7 +147,20 @@ module.exports = function loader() {
     .then(src => callback(null, src))
     .catch(err => {
       if (err instanceof ChildProcessError) {
-        err.message += `\n\nStdout follows:\n${err.stdout}'\n\nStderr follows:${err.stderr}`;
+        err.message += `\n\nStdout follows:\n${err.stdout}'\n\nStderr follows:\n${err.stderr}`;
+
+        if (err.code === 23) {
+          callback(
+            new Error(
+              '\n\n' +
+                chalk.red(
+                  "bs-loader likely did not find one of the paths listed in bsconfig.json 'sources'\n\n"
+                ) +
+                err.message
+            )
+          );
+          return;
+        }
 
         const errorMessages = getBsbErrorMessages(err.message);
         if (errorMessages) {
@@ -155,6 +169,6 @@ module.exports = function loader() {
         }
       }
 
-      throw err;
+      callback(err);
     });
 };
