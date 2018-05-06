d27d0046ec786a2ce58e29bf033c3c3846564237
Paul Snyder <snyderpa@gmail.com>
2018-05-06 12:02:03 -0700

    fix the not-found-file error

--- packages/react-scripts/lib/createJestConfig.js
+++ packages/react-scripts/lib/createJestConfig.js
@@ -11,7 +11,7 @@
 
 const fs = require('fs');
 const chalk = require('chalk');
-const paths = require('../../config/paths');
+const paths = require('../config/paths');
 
 module.exports = (resolve, rootDir) => {
   // Use this instead of `paths.testsSetup` to avoid putting
